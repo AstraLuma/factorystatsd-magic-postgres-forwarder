9c98ff82f3d0b778cc48d9f747d1c8e2bb708d30
Jamie Bliss <jamie@ivyleav.es>
2022-10-12 17:00:18 -0400

    mock-factory.py: use synthetic clock instead of system

--- mock-factory.py
+++ mock-factory.py
@@ -4,6 +4,7 @@ Script to just copy the mock JSON into a script output directory.
 
 Uses $SCRIPT_OUTPUT
 """
+import itertools
 import json
 import os
 from pathlib import Path
@@ -18,8 +19,7 @@ dest = Path(os.environ['SCRIPT_OUTPUT']).absolute()
 
 print("Emitting metadata", flush=True)
 shutil.copy(meta, dest)
-start = time.monotonic()
-while True:
-    sample_data['ticks'] = (time.monotonic() - start) * 60
+for tick in itertools.count(0, 60):
+    sample_data['ticks'] = tick
     (dest / 'factorystatsd-samples.json').write_text(json.dumps(sample_data))
     time.sleep(1)
