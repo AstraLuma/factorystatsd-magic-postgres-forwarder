045b880d72c18ab70819f932d699436a31cffcb5
Jamie Bliss <jamie@ivyleav.es>
2022-12-16 21:20:33 -0500

    Use mtime checking with samples data

--- pg_magic/incoming.py
+++ pg_magic/incoming.py
@@ -18,6 +18,22 @@ def _read_and_nuke(file: Path):
     return data
 
 
+class Checker:
+    def __init__(self, path):
+        self.path = Path(path)
+        self.last_mtime = 0
+
+    def __call__(self):
+        try:
+            mod_time = self.path.stat().st_mtime
+        except FileNotFoundError:
+            return
+        else:
+            if mod_time > self.last_mtime:
+                self.last_mtime = mod_time
+                return json.loads(self.path.read_text())
+
+
 def read_factorio(script_output: Path) -> Iterable[Tuple[str, dict]]:
     """
     Reads the data from the in-game factorystatsd mod.
@@ -26,30 +42,21 @@ def read_factorio(script_output: Path) -> Iterable[Tuple[str, dict]]:
     * kind: string of either "meta" or "samples"
     * data: The actual blob
     """
-    meta_path = script_output / 'factorystatsd-game-data.json'
-    samples_path = script_output / 'factorystatsd-samples.json'
-
-    last_meta_mod_time = 0
+    meta = Checker(script_output / 'factorystatsd-game-data.json')
+    samples = Checker(script_output / 'factorystatsd-samples.json')
 
     while True:
         try:
-            try:
-                meta_mod_time = meta_path.stat().st_mtime
-            except FileNotFoundError:
-                pass
-            else:
-                if meta_mod_time > last_meta_mod_time:
-                    LOG.info('loading game data')
-                    yield 'meta', json.loads(meta_path.read_text())
-                    last_meta_mod_time = meta_mod_time
+            meta_data = meta()
+            if meta_data is not None:
+                LOG.info('loaded game data')
+                yield 'meta', meta_data
 
-            try:
-                samples = _read_and_nuke(samples_path)
-            except FileNotFoundError:
-                LOG.debug('No samples data yet')
-            else:
+            sample_data = samples()
+            if sample_data is not None:
                 LOG.info('loaded samples data')
-                yield "samples", samples
+                yield "samples", sample_data
+
         except Exception as e:
             LOG.exception(f'forwarder exception: {e}')
             time.sleep(1.0)
